ef3493885b718b8d9c99e65404e2f20f770b1072
Milind Jain <jainmilind10112002@gmail.com>
2023-02-28 11:11:42 +0530

    Made Code Compactible overall

--- Trie.py
+++ Trie.py
@@ -1,30 +1,30 @@
-class Trie : 
-    def __init__(self) : 
+class Trie:
+    def __init__(self): 
         self.child = dict()
         self.end = False
         self.cnt = 0
         
-    def insert(self , word) : 
+    def insert(self, word): 
         root = self
-        for char in word :
-            if root.child.get(char) == None :
+        for char in word:
+            if root.child.get(char) == None:
                 root.child[char] = Trie()
             root = root.child[char]
         root.cnt += 1
         root.end = True
     
-    def wildcard(self , word) : 
-        ans , cur = set() , word
+    def wildcard(self, word): 
+        ans = set()
         root = self
-        for char in word : 
-            if root.child.get(char) == None : 
+        for char in word: 
+            if root.child.get(char) == None: 
                 return ans
             root = root.child[char]
-        def dfs(node , cur) :
-            if node.end == True : 
+        def dfs(node, cur):
+            if node.end == True: 
                 ans.add(cur)
-            for label in node.child : 
+            for label in node.child: 
                 newstr = cur + label
-                dfs(node.child[label] , newstr)
-        dfs(root , word)
+                dfs(node.child[label], newstr)
+        dfs(root, word)
         return ans
